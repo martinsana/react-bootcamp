7fc5691d9633338847187193e3f075b8db23f715
Ana Paula <anapaula.martins17@hotmail.com>
2021-06-14 23:42:47 -0300

    useState example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+
 export default function App() {
-  console.log('Teste no console do navegador');
+  const [name, setname] = useState('Ana');
+
+  function handleNameChange(event) {
+    const newName = event.currentTarget.value;
+    setname(newName);
+  }
 
   return (
     <>
-      <header>
-        <div className="bg-gray-100 mx-auto p-4">
-          <h1 className="text-center font-semibold text-xl">
-            Projeto base para o Módulo React I
-          </h1>
+      <Header size="large">React-Hello</Header>
+      <Main>
+        <div className="flex flex-col">
+          <label className="mb-1" htmlFor="inputName">
+            Type name:
+          </label>
+          <input
+            autoFocus
+            className="p-1 border "
+            id="inputName"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
         </div>
-      </header>
 
-      <main>
-        <div className="container mx-auto p-4">
-          <h2>O conteúdo fica aqui.</h2>
-        </div>
-      </main>
+        <p>
+          My name is: {name}, with {name.length} chars. I am 26 years old.
+        </p>
+      </Main>
     </>
   );
 }
